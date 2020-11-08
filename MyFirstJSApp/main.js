1ad0814a71df530a856df092267d083765382256
II03uTuB <sir.med2012@gmail.com>
2020-11-08 12:32:19 +0300

    Chore acquaintance with let and for

--- MyFirstJSApp/main.js
+++ MyFirstJSApp/main.js
@@ -1,1 +1,14 @@
 console.log('egebr')
+
+let message;
+message = 'Hello'
+console.log(message)
+let miss = 'Hello, miss'
+console.log(miss)
+let one=1,
+    two=2,
+    three=3;
+for(let index = 0; index < 3; index++) {
+    console.log(index);
+    //index++;
+}
