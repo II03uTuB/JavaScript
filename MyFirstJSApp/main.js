6df145fce256bac58a76d4c49c0eeb4612f62989
II03uTuB <sir.med2012@gmail.com>
2020-11-08 13:39:07 +0300

    Chore: Learn BigInt and Number type

--- MyFirstJSApp/main.js
+++ MyFirstJSApp/main.js
@@ -1,14 +1,26 @@
-console.log('egebr')
+//console.log('egebr')
 
 let message;
 message = 'Hello'
-console.log(message)
+//console.log(message)
 let miss = 'Hello, miss'
-console.log(miss)
+//console.log(miss)
 let one=1,
     two=2,
     three=3;
 for(let index = 0; index < 3; index++) {
-    console.log(index);
-    //index++;
+    //console.log(index);
+    index++;
 }
+const COLOR_RED = "#F00";
+let color = COLOR_RED;
+//console.log(color)
+let someValue = "reqh";
+console.log(someValue);
+someValue = 1234;
+console.log(someValue);
+
+console.log(1 / 0);
+console.log(32);
+let bigInt = 897654345678988876543567897654n;
+console.log(bigInt);
