69b2469577d9a6d0fae966858ab579875e35e450
II03uTuB <sir.med2012@gmail.com>
2020-11-09 18:02:55 +0300

    Chore: Check functions

--- MyFirstJSApp/browser.js
+++ MyFirstJSApp/browser.js
@@ -1,3 +1,13 @@
-let age = prompt('How old are you?');
-let result = confirm("Are you here?");
-alert(confirm("Are you here?"));
+//let age = prompt('How old are you?');
+//let result = confirm("Are you here?");
+//alert(confirm("Are you here?"));
+
+let someValue = true;
+//alert(typeof someValue);
+
+someValue = String(someValue);
+//alert("32" + "32")
+
+let int = "123gr"
+console.log(Number(int))
+console.log(Number("32" - "mat"));
